98889b7df93bae0d01838de2635cdc3c6807457d
AkhilaGeorge4039 <157182953+AkhilaGeorge4039@users.noreply.github.com>
2024-04-18 20:56:03 -0400

    Update Home.jsx

--- group1_finalproject/src/components/Home.jsx
+++ group1_finalproject/src/components/Home.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { ReactTyped } from 'react-typed';
-import { Link } from 'react-router-dom';
-import '../css/Home.css';
+import { ReactTyped } from 'react-typed'; // Importing ReactTyped for typing effect
+import { Link } from 'react-router-dom'; // Importing Link from react-router-dom for navigation
+import '../css/Home.css'; // Importing CSS file for Home component
 
 function Home() {
   return (
+    // Home container
     <div className="home-container">
+      {/* Background image */}
       <div className="bg-image" />
+      {/* Jumbotron */}
       <div className="jumbotron">
         <div className="intro-content">
           <div className="container">
+            {/* Title */}
             <h1 className="intro-title mb-4">Welcome to My Portfolio</h1>
+            {/* Subtitle with typing effect */}
             <p className="intro-subtitle">
+              {/* Container for name */}
               <div className="name-container">
+                {/* Name */}
                 <p className="name">Pritesh Patel</p>
               </div>
+              {/* Typing effect */}
               <span className="text-slider-items"></span>
               <strong className="text-slider">
                 <ReactTyped
@@ -30,6 +38,7 @@ function Home() {
                 />
               </strong>
             </p>
+            {/* Additional information link */}
             <div className="additional-info">
               <Link to="/about" className="btn btn-primary">For More Info :) </Link>
             </div>
